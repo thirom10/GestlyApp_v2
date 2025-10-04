4871308994d70d052aae16f6889cb2e8f03d9aec
Ezequiel <ezepelo1406@gmail.com>
2025-10-03 20:15:23 -0600

    Corregi el error de los botones en Settings
    al final pude solucionarlo, ahora son funcionales los botones de Settings, falta desarrollar las pantallas de cada seccion.

--- src/screens/Settings/SettingsNavigator.tsx
+++ src/screens/Settings/SettingsNavigator.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import { Colors } from '../../shared/config/colors';
 import AccountScreen from './AccountScreen';
 import SettingsScreen from './SettingsScreen';
 import SubscriptionScreen from './SubscriptionScreen';
@@ -13,19 +14,53 @@ export default function SettingsNavigator() {
     <Stack.Navigator
       initialRouteName="Settings"
       screenOptions={{
-        headerShown: false,
-        presentation: 'card',
+        headerStyle: {
+          backgroundColor: Colors.background,
+        },
+        headerTintColor: Colors.textPrimary,
+        headerTitleStyle: {
+          fontWeight: '600',
+          fontSize: 18,
+        },
+        contentStyle: {
+          backgroundColor: Colors.background,
+        },
+        headerShadowVisible: true,
       }}
     >
-      <Stack.Screen name="Settings" component={SettingsScreen} />
-      <Stack.Screen name="Account" component={AccountScreen} />
-      <Stack.Screen name="Subscription" component={SubscriptionScreen} />
+      <Stack.Screen 
+        name="Settings" 
+        component={SettingsScreen}
+        options={{
+          title: 'Usuario',
+        }}
+      />
+      
+      <Stack.Screen 
+        name="Account" 
+        component={AccountScreen}
+        options={{
+          title: 'Información de cuenta',
+          headerBackTitle: 'Atrás',
+        }}
+      />
+      
+      <Stack.Screen 
+        name="Subscription" 
+        component={SubscriptionScreen}
+        options={{
+          title: 'Gestionar Suscripción',
+          headerBackTitle: 'Atrás',
+        }}
+      />
+      
       <Stack.Screen
         name="AppearanceModal"
         component={AppearanceModal}
         options={{
           presentation: 'modal',
           animation: 'slide_from_bottom',
+          title: 'Cambiar Apariencia',
         }}
       />
     </Stack.Navigator>
